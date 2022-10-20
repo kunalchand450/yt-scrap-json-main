fe41de809b63c0512e4f1f0fb34634643ae3e7c3
kunal chand <81466590+kunalchand450@users.noreply.github.com>
2022-10-20 10:22:21 +0530

    Update settings.py

--- scrappers/webscrapers/settings.py
+++ scrappers/webscrapers/settings.py
@@ -15,7 +15,7 @@ NEWSPIDER_MODULE = 'webscrapers.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'webscrapers (+http://www.yourdomain.com)'
-USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+# USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
